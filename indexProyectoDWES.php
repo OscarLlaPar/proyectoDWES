ad40562a5b7ea6583af27fe1a1cdb402ee2b72a3
oscar <oscar.llapar@educa.jcyl.es>
2021-10-15 10:25:16 +0200

    github nuevo

---
+++ indexProyectoDWES.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Desarrollo Web en Entorno Servidor</title>
+        <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+            <div class="titulo">
+                <h1>Desarrollo Web en Entorno Servidor</h1>
+                <h2>Óscar Llamas Parra</h2>
+                <div class="cuadro" id="arriba"></div>
+            </div>
+        <div class="cont">
+            <p>Tema 1 — Desarrollo Web en Entorno Servidor</p>
+            <p>Tema 2 — Instalación, Configuración y Documentación del Entorno de Desarrollo y el Entorno de Explotación</p>
+            <p><a href="../ProyectoTema3/indexProyectoTema3.php">Tema 3 — Características del Lenguaje PHP</a></p>
+            <p>Tema 4 — Técnicas de Acceso a Datos en PHP</p>
+            <p>Tema 5 — Desarrollo de Aplicaciones Web Utilizando Código Embebido</p>
+            <p>Tema 6 — Programación Orientada a Objetos en PHP</p>
+            <p>Tema 7 — Programación de Servicios Web</p>
+            <p>Tema 8 — Desarrollo de Aplicaciones Web Híbridas</p>
+        </div>
+			
+    </body>
+    <footer>
+        <p>
+            Óscar Llamas Parra
+			<a href="https://github.com/OscarLlaPar/" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
+        </p>
+        <p>
+            DAW 2
+        </p>
+        <p>
+            IES Los Sauces, Benavente 2021-2022
+        </p>
+        <div class="cuadro" id="abajo"></div>
+    </footer>
+</html>
