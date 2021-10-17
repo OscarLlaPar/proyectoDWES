9a14600f4cc85e9a3884474a15a710f3e6944a4f
OscarLlaPar <74042618+OscarLlaPar@users.noreply.github.com>
2021-10-18 00:53:34 +0200

    17/10/2021: DWES temas colocados

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -11,27 +11,43 @@ and open the template in the editor.
         <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-            <div class="titulo">
-                <h1>Desarrollo Web en Entorno Servidor</h1>
-                <h2>Óscar Llamas Parra</h2>
-                <div class="cuadro" id="arriba"></div>
-            </div>
-        <div class="cont">
-            <p>Tema 1 — Desarrollo Web en Entorno Servidor</p>
-            <p>Tema 2 — Instalación, Configuración y Documentación del Entorno de Desarrollo y el Entorno de Explotación</p>
-            <p><a href="../ProyectoTema3/indexProyectoTema3.php">Tema 3 — Características del Lenguaje PHP</a></p>
-            <p>Tema 4 — Técnicas de Acceso a Datos en PHP</p>
-            <p>Tema 5 — Desarrollo de Aplicaciones Web Utilizando Código Embebido</p>
-            <p>Tema 6 — Programación Orientada a Objetos en PHP</p>
-            <p>Tema 7 — Programación de Servicios Web</p>
-            <p>Tema 8 — Desarrollo de Aplicaciones Web Híbridas</p>
+        
+        <div class="titulo">
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <h2>Óscar Llamas Parra</h2>
+            <div class="cuadro" id="arriba"></div>
         </div>
-			
+        <div class="cont">
+            <div class="tema">
+                <p>Tema 1 — Desarrollo Web en Entorno Servidor</p>
+            </div>
+            <div class="tema">
+                <p>Tema 2 — Instalación, Configuración y Documentación del Entorno de Desarrollo y el Entorno de Explotación</p>
+            </div>
+            <div class="tema">
+                <p><a href="../ProyectoTema3/indexProyectoTema3.php">Tema 3 — Características del Lenguaje PHP</a></p>
+            </div>
+            <div class="tema">
+                <p>Tema 4 — Técnicas de Acceso a Datos en PHP</p>
+            </div>
+            <div class="tema">
+                <p>Tema 5 — Desarrollo de Aplicaciones Web Utilizando Código Embebido</p>
+            </div>
+            <div class="tema">
+                <p>Tema 6 — Programación Orientada a Objetos en PHP</p>
+            </div>
+            <div class="tema">
+                <p>Tema 7 — Programación de Servicios Web</p>
+            </div>
+            <div class="tema">
+                <p>Tema 8 — Desarrollo de Aplicaciones Web Híbridas</p>
+            </div>
+        </div>
     </body>
     <footer>
         <p>
             Óscar Llamas Parra
-			<a href="https://github.com/OscarLlaPar/" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
+            <a href="https://github.com/OscarLlaPar/" target="__blank"><img src="webroot/img/github.png" alt="Github"></img></a>
         </p>
         <p>
             DAW 2
